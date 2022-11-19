03a71ed569a06e56b738dbc7298eb1c3384b5f5f
ABeliy <andreybelobra2010@gmail.com>
2022-11-19 21:10:27 +0400

    CORS fixing 2

--- server/server.js
+++ server/server.js
@@ -13,6 +13,15 @@ const connectDB = require('./config/dbConn');
 
 connectDB();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 app.use(credentials);
