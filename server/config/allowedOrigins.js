45d9da7ececc481ee329e636f903c799c40ed10b
ABeliy <andreybelobra2010@gmail.com>
2022-11-20 16:27:26 +0400

    http://localhost:3000

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,10 +1,8 @@
 const allowedOrigins = [
-  '*',
-  'https://slogger.netlify.app',
+  'http://slogger.netlify.app',
   'http://127.0.0.1',
   'http://localhost:3500',
   'http://localhost:3000',
-
 ]
 
 module.exports = allowedOrigins;
