3028b3d69ff08b86c70ead8400d50148bbf66319
ABeliy <andreybelobra2010@gmail.com>
2022-11-20 20:59:42 +0400

    headers:Access-Control-Allow-Origin: *

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,7 +1,8 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-  origin: (origin, callback) => {
+  origin: 
+  (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
@@ -9,7 +10,8 @@ const corsOptions = {
     }
   },
   optionsSuccessStatus: 200,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  headers:'Access-Control-Allow-Origin: *'
 };
 
 module.exports = corsOptions;
