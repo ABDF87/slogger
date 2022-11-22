15bf901b7f56d1a07105b6401e063dce62d5d0a0
ABeliy <andreybelobra2010@gmail.com>
2022-11-22 14:50:19 +0400

    devtools disabled

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from '../src/components/context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
